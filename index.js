51d26bd36782c90358c4aa5e460cb5314d760c1a
CharAznable777 <evandrodbz@hotmail.com>
2025-11-22 19:31:43 -0300

    desafio do heroi

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("oi");
+let nomeHeroi = "aznable";
+let experiencia = 7000;
+
+let nivel;
+
+if (experiencia < 1000) nivel = "Ferro";
+else if (experiencia <= 2000) nivel = "Bronze";
+else if (experiencia <= 5000) nivel = "Prata";
+else if (experiencia <= 7000) nivel = "Ouro";
+else if (experiencia <= 8000) nivel = "Platina";
+else if (experiencia <= 9000) nivel = "Ascendente";
+else if (experiencia <= 10000) nivel = "Imortal";
+else categoria = "Radiante";
+
+
+console.log("O herói " + nomeHeroi + " está no nível: " + nivel);
